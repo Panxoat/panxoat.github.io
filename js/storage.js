f02126b1a9611312f1b9e924750fee6ab96be9ed
Panxoat <a20304645@gmail.com>
2023-12-08 15:51:54 +0900

    feat: todo remove

--- js/storage.js
+++ js/storage.js
@@ -5,47 +5,68 @@ const todoListContainer = document.getElementById("todo_list_container");
 const todoKey = `${new Date().getMonth() + 1}/${new Date().getDay()}`;
 
 window.onload = function () {
-  const prevTodo = getTodo();
+  const prevTodoList = getTodoList();
 
-  prevTodo.forEach((todo) => {
-    const todoList = document.createElement("li");
-    todoList.append(todo);
-    todoListContainer.appendChild(todoList);
+  prevTodoList.forEach((todo) => {
+    paintTodo(todo);
   });
 };
 
-function handleTodoInput(e) {
-  e.preventDefault();
-
-  saveTodo(todoInput?.value || "", () => {
-    todoInput.value = "";
-  });
-}
-
-function saveTodo(todo, resetValue) {
-  if (todo) {
-    const prevTodo = getTodo();
-    const isExistTodo = prevTodo.find((todo_) => todo_ === todo);
-
-    if (!isExistTodo) {
-      window.localStorage.setItem(todoKey, JSON.stringify([...prevTodo, todo]));
-      paintTodo(todo, resetValue);
-    }
-  }
-}
-
-function getTodo() {
+function getTodoList() {
   const prevTodo = JSON.parse(window.localStorage.getItem(todoKey) || "[]");
   return prevTodo;
 }
 
-function paintTodo(todo, resetValue) {
-  const todoList = document.createElement("li");
+function setTodoList(todoList) {
+  window.localStorage.setItem(todoKey, JSON.stringify(todoList));
+}
 
-  todoList.append(todo);
-  todoListContainer.appendChild(todoList);
+function deleteTodo(e) {
+  const list = e.target.parentNode;
+  const span = list.firstChild;
 
-  resetValue();
+  const prevTodoList = getTodoList();
+  const filteredTodo = prevTodoList.filter((todo_) => todo_ !== span.innerText);
+
+  setTodoList(filteredTodo);
+  list.remove();
+}
+
+function paintTodo(todo) {
+  const listElement = document.createElement("li");
+  const spanElement = document.createElement("span");
+  const todoRemoveButton = document.createElement("button");
+  todoRemoveButton.setAttribute("type", "button");
+
+  todoRemoveButton.addEventListener("click", (e, todo) => {
+    return deleteTodo(e, todo);
+  });
+
+  spanElement.innerText = todo;
+  listElement.appendChild(spanElement);
+  todoRemoveButton.append("X");
+
+  listElement.appendChild(todoRemoveButton);
+  todoListContainer.appendChild(listElement);
+}
+
+function preprocessTodo(todo) {
+  if (todo) {
+    const prevTodoList = getTodoList();
+    const isExistTodo = prevTodoList.find((todo_) => todo_ === todo);
+
+    if (!isExistTodo) {
+      setTodoList([...prevTodoList, todo], todo);
+      paintTodo(todo);
+    }
+  }
+}
+
+function handleTodoInput(e) {
+  e.preventDefault();
+
+  preprocessTodo(todoInput?.value || "");
+  todoInput.value = "";
 }
 
 form.addEventListener("submit", handleTodoInput);
